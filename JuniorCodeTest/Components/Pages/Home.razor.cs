eec9512735af88613ccebcf436d46eb83b64b399
Ben Balls <b3njamin.balls@gmail.com>
2024-03-04 12:55:43 +0000

    pushing to github

---
+++ JuniorCodeTest/Components/Pages/Home.razor.cs
@@ -0,0 +1,41 @@
+﻿using JuniorCodeTest.Interfaces;
+using JuniorCodeTest.Models;
+
+using Microsoft.AspNetCore.Components;
+
+
+namespace JuniorCodeTest.Components.Pages
+{
+    public partial class Home : ComponentBase
+    {
+        public List<RequestedUsersModel> RandomUsers { get; set; } = [];
+
+        [Inject]
+        private IRandomUserApiService? RandomUserApiService { get; set; }
+
+
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+
+            try
+            {
+                await PopulateUserList();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        async Task PopulateUserList()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                var users = await RandomUserApiService.GetRandomUserDataFromApi();
+                RandomUsers.AddRange(users);
+            }
+
+        }
+    }
+}
